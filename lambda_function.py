ef6cc77da265b1d77a5282fa2138245bc8a672bb
mxcheung <mxcheung@users.noreply.github.com>
2023-04-29 21:50:14 +1000

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -8,12 +8,14 @@ logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     # TODO implement
+    DB_HOST = os.environ['DB_HOST']
     json_region = os.environ['AWS_REGION']
     logger.info('AWS Region of %s', json_region)
     response = get_response(event)
     return {
         'statusCode': 200,
         'region': json_region,
+        'DB_HOST': DB_HOST,
         'body': json.dumps(response)
     }
 
