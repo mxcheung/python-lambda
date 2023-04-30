6bf94a8ccfae3ad90b7c134411151622cc4c57b7
mxcheung <mxcheung@users.noreply.github.com>
2023-04-30 17:05:33 +1000

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,3 +1,15 @@
 sudo yum install python-pip
 pip install awsebcli --upgrade --user
 pip install camelcase
+
+
+============================
+# This program prints Hello, world!
+
+from camelcase import CamelCase
+c = CamelCase()
+s = 'this is a sentence that needs CamelCasing!'
+print(c.hump(s))
+# This is a Sentence That Needs CamelCasing!
+
+print('Hello, world!')
