732375bd7a2b4ad20784dabf91fe0d8abcb27318
mxcheung <mxcheung@users.noreply.github.com>
2023-06-13 07:55:05 +1000

    Update botosession.py

--- dynamodb/botosession.py
+++ dynamodb/botosession.py
@@ -15,3 +15,28 @@ instances = ec2_client.describe_instances()
 # Rest of your code...
 
 Connection was closed before we received a valid response from endpoint URL: "https://s3.amazonaws.com/".
+  
+  
+  import boto3
+
+# Set the AWS credentials
+aws_access_key_id = 'your_access_key_id'
+aws_secret_access_key = 'your_secret_access_key'
+
+# Create a session with the specified credentials
+session = boto3.Session(
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key
+)
+
+# Use the session to create AWS service clients
+s3_client = session.client('s3')
+dynamodb_client = session.client('dynamodb')
+
+# Now you can use the AWS service clients to interact with AWS
+# For example:
+response = s3_client.list_buckets()
+tables = dynamodb_client.list_tables()
+
+# Rest of your code...
+
