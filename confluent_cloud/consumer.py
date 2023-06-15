ac5ea785e7767d1f9c393aa60877ec1c117cbcd9
mxcheung <mxcheung@users.noreply.github.com>
2023-06-16 08:46:33 +1000

    Create consumer.py

---
+++ confluent_cloud/consumer.py
@@ -0,0 +1,41 @@
+from confluent_kafka import Consumer, KafkaException, KafkaError
+
+def message_handler(message):
+    if message.error():
+        print(f"Error: {message.error()}")
+    else:
+        print(f"Received message: {message.value().decode('utf-8')}")
+
+conf = {
+    'bootstrap.servers': '<bootstrap.servers>',
+    'security.protocol': 'SASL_SSL',
+    'sasl.mechanism': 'PLAIN',
+    'sasl.username': '<username>',
+    'sasl.password': '<password>',
+    'group.id': '<group.id>',
+    'auto.offset.reset': 'earliest'
+}
+
+consumer = Consumer(conf)
+topic = '<topic>'
+consumer.subscribe([topic])
+
+try:
+    while True:
+        message = consumer.poll(timeout=1.0)
+        if message is None:
+            continue
+        if message.error():
+            if message.error().code() == KafkaError._PARTITION_EOF:
+                continue
+            else:
+                print(f"Error: {message.error()}")
+                break
+        else:
+            message_handler(message)
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    consumer.close()
